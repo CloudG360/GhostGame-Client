85d6a9f72400aebb24efc159ff0c9cbed7d7df9e
Will <16578982+CloudG360@users.noreply.github.com>
2021-09-06 21:25:15 +0100

    Upgraded Check to its java counterpart

--- Assets/Scripts/me/cg360/spookums/utility/Check.cs
+++ Assets/Scripts/me/cg360/spookums/utility/Check.cs
@@ -4,27 +4,60 @@ namespace me.cg360.spookums.utility
 {
     public class Check
     {
-        public static void MissingProperty(Object obj, String loc, String name) {
+        /**
+         * For use in json, config, and Data API
+         * situations where a specific property that is required
+         * is missing.
+         *
+         * @param obj - The object which represents the property that is missing.
+         * @param loc - A string descriptor of the holding object.
+         * @param name - The name of the property.
+         *
+         * @return the parameter "obj"
+         */
+        public static T MissingProperty<T>(T obj, String loc, String name) {
             if(IsNull(obj)) throw new InvalidOperationException($"'{loc}' is missing a valid '{name}' property.");
-        }
-
-        public static void NullParam(Object obj, String name) {
-            if(IsNull(obj)) throw new ArgumentNullException($"'{name}' cannot be null.");
+            return obj;
         }
 
         /**
-     * Assets that a value is between two numbers.
-     * @param val the value being checked.
-     * @param lowerBound the lower bound checked (inclusive)
-     * @param upperBound the upper bound checked (inclusive)
-     * @param name the name of the variable/property.
-     */
-        public static void InclusiveBounds(int val, int lowerBound, int upperBound, String name) {
-            if((val < lowerBound)) throw new InvalidOperationException($"{name} is out of bounds (val = {val} | Lower = {lowerBound})");
-            if((val > upperBound)) throw new InvalidOperationException($"{name} is out of bounds (val = {val} | Upper = %{upperBound})");
+         * Checks if a parameter is null in a cleaner way, throwing an IllegalArgumentException if true.
+         * @param obj - the parameter that is potentially null.
+         * @param name - the name of the parameter.
+         *
+         * @return the parameter "obj"
+         */
+        public static T NullParam<T>(T obj, String name) {
+            if(IsNull(obj)) throw new ArgumentNullException($"'{name}' cannot be null.");
+            return obj;
         }
 
-        public static bool IsNull(object obj) {
+        /**
+         * Assets that a value is between two numbers.
+         * @param val the value being checked.
+         * @param lowerBound the lower bound checked (inclusive)
+         * @param upperBound the upper bound checked (inclusive)
+         * @param name the name of the variable/property.
+         *
+         * @return the parameter "val"
+         */
+        public static int InclusiveBounds(int val, int lowerBound, int upperBound, String name) {
+            InclusiveLowerBound(val, lowerBound, name);
+            InclusiveUpperBound(val, upperBound, name);
+            return val;
+        }
+
+        public static int InclusiveLowerBound(int val, int bound, String name) {
+            if(val < bound) throw new InvalidOperationException($"{name} is out of bounds (val = {val} | Lower = {bound})");
+            return val;
+        }
+
+        public static int InclusiveUpperBound(int val, int bound, String name) {
+            if(val > bound) throw new InvalidOperationException($"{name} is out of bounds (val = {val} | Upper = %{bound})");
+            return val;
+        }
+
+        public static bool IsNull(Object obj) {
             return obj == null;
         }
     }
