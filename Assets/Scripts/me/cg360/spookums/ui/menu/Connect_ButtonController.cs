626c6c3894291fa8f45f7205b7972c8f903ab6f6
Will <16578982+CloudG360@users.noreply.github.com>
2021-09-07 00:00:25 +0100

    Add two new UI scripts

---
+++ Assets/Scripts/me/cg360/spookums/ui/menu/Connect_ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Connect_ButtonController : MonoBehaviour
+{
+    public Button Button;
+    void Start()
+    {
+        Button.onClick.AddListener(ClickedButton);
+    }
+
+    void ClickedButton()
+    {
+        
+    }
+}
