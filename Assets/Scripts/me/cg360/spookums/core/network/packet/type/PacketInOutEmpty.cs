8bc77890f44ce106c70bb8e55fb812fa3839e95d
Will <16578982+CloudG360@users.noreply.github.com>
2021-12-04 13:30:57 +0000

    Sort-of-fixed empty packets not being recieved correctly. It's hacky but it works lmao

--- Assets/Scripts/me/cg360/spookums/core/network/packet/type/PacketInOutEmpty.cs
+++ Assets/Scripts/me/cg360/spookums/core/network/packet/type/PacketInOutEmpty.cs
@@ -3,7 +3,13 @@
     public abstract class PacketInOutEmpty : NetworkPacket
     {
 
-        protected sealed override ushort EncodeBody() { return 0; }
+        protected sealed override ushort EncodeBody()
+        {
+            // Hacky fix to get empty packets to send
+            // I don't have the time to fix it right now but this defo needs fixing!
+            Body.PutUnsignedByte(0x00);
+            return 1;
+        }
         protected sealed override void DecodeBody(ushort inboundSize) { }
     }
 }
